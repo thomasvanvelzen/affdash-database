de33e1ea4f85e974410c8479faaa4dead902b367
Thomas van Velzen <50317402+thomasvanvelzen@users.noreply.github.com>
2024-04-10 14:26:29 +0200

    Update smart-link.ts

--- schemas/smart-link.ts
+++ schemas/smart-link.ts
@@ -1,8 +1,10 @@
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { offers } from "./offer";
+import { users } from "./user";
 
 export const smartLinks = sqliteTable("smartLinks", {
   id: text("id").primaryKey(),
+  userId: text("userId").notNull().references(() => users.id),
   name: text("name").notNull(),
   offerId: text("offerId")
     .notNull()
